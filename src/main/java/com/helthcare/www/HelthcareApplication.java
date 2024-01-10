22f5efeb59d550ef7bb9ddf780fcb81c65a2ac02
HANGYEOL\ffink <ffinky99@gmail.com>
2024-01-10 15:03:56 +0900

    z

---
+++ src/main/java/com/helthcare/www/HelthcareApplication.java
@@ -0,0 +1,13 @@
+package com.helthcare.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelthcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helthcare/www/HelthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helthcare.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
